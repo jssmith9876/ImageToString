abe4effeaaf7f87d1551cc6a6c255698166159d4
jssmith9876 <jssmith9876@yahoo.com>
2019-11-22 10:50:48 -0800

    Cleaned up by removing useless comments

--- ImageParser.py
+++ ImageParser.py
@@ -5,17 +5,12 @@ import os
 if __name__ == '__main__':
     file_location = sys.argv[1]
 
-    # Convert the image to greyscale and save it 
-    # img = Image.open(file_location).convert('LA')
-    # img.save('greyscale.png')
-
     img = Image.open(file_location).convert('L')
     WIDTH, HEIGHT = img.size
     
     data = list(img.getdata())
     data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH * HEIGHT, WIDTH)]
 
-    #print(data)
 
     #Open the text file to write the data to
     file = open('outputFile.txt', 'w')
